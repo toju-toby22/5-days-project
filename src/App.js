daae14867a8370491e85be4c66754719864a2022
Tobi Otoju <ttotoju@gmail.com>
2023-05-03 19:58:12 +0100

    added components nd folders

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Homepage from './pages/Homepage';
+import {  Navigate, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Jumia
-        </a>
-      </header>
+    <div>
+      
+          <Routes>
+            <Route path="/"  element={ <Homepage />} />
+            {/* <Route path="/Auth"  element={ <Auth />} />
+            <Route path="Answer" element={<Answer />} /> */}
+            
+          </Routes>
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from "../components/homepage/HomePage-components"
+
+
+
+const Homepage = () => {
+  return (
+    <HomePage/>
+  )
+}
+
+export default Homepage;
